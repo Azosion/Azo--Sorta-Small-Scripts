abe61ba52454b555a25a49ee5d227d34d5c94cef
Azosion <67764087+Azosion@users.noreply.github.com>
2025-02-12 18:43:45 -0800

    Add files via upload

---
+++ Azo-scripts.plugin.zsh
@@ -0,0 +1,50 @@
+
+#######################################
+#      Azo's Sorta tiny scripts       #
+#######################################
+
+function paco() {
+# Get the list of orphaned packages
+orphaned_packages=$(pacman -Qdtq)
+random_number=$((RANDOM % 10 + 1))
+
+# Check if there are any orphaned packages
+if [ -z "$orphaned_packages" ]; then
+    echo "No oprhans found"
+else
+    # First pass: Remove orphaned packages (with user confirmation)
+    echo "The following orphaned packages will be removed:"
+    echo "$orphaned_packages"
+    echo "Do you want to proceed? [y/N]"
+    read -r user_input
+
+    if [[ "$user_input" =~ ^[Yy]$ || -z "$user_input" ]]; then
+        # User confirmed, proceed with the first pass
+        sudo pacman -Rs $(pacman -Qtdq)
+
+        # Check if the first pass was successful
+        if [ $? -eq 0 ]; then
+            # Second pass: Remove any remaining orphaned packages (non-interactive)
+            remaining_orphaned_packages=$(pacman -Qdtq)
+            echo remaining_orphaned_packages
+            if [ -n "$remaining_orphaned_packages" ]; then
+                echo "Running second pass to purge remaining orphans..."
+                sudo pacman -Rs --noconfirm $(pacman -Qtdq)
+            fi
+        fi
+
+        echo "Oprhans purged!"
+         if [ "$random_number" -eq 1 ]; then
+      echo "you monster"
+    fi
+    else
+        # User declined, exit without doing anything
+        echo "Operation canceled."
+    fi
+fi
+}
+
+alias c='clear'
+
+alias e='exit'
+
